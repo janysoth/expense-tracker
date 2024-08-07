1a049d249e01f9bb9c26b80e35eae88cc15d2e1a
Jonny Vorn Soth <janysoth@gmail.com>
2024-08-07 01:37:04 -0500

    Add transactions function in Query.

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,6 +1,22 @@
+import Transaction from "../models/transaction.model.js";
+import User from "../models/user.model.js";
 
 const transactionResolver = {
-  Query: {},
+  Query: {
+    transactions: async (_, __, context) => {
+      try {
+        if (!context.getUser()) throw new Error("Unauthorized");
+        const userId = await context.getUser()._id;
+
+        const transactions = await Transaction.find({ userId });
+
+        return transactions;
+      } catch (err) {
+        console.error("Error getting transactions: ", err);
+        throw new Error("Error getting transactions");
+      }
+    }
+  },
   Mutation: {},
 };
 

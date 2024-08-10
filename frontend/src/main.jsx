8f6e86e6333070191dbb47114e7d7e4831b09359
Jonny Vorn Soth <janysoth@gmail.com>
2024-08-10 11:13:40 -0500

    Remove some typos.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -21,7 +21,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <ApolloProvider client={client}>
           <App />
         </ApolloProvider>
-        <App />
       </GridBackground>
     </BrowserRouter>
   </React.StrictMode>
